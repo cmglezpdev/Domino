760dcdc4b355c669dff8966c6251f112a1f3d51d
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-06-03 22:51:50 -0400

    feat/clinet:Add borad

--- Client/src/components/Game/Game.jsx
+++ Client/src/components/Game/Game.jsx
@@ -8,12 +8,18 @@ export const Game = ({ settings }) => {
   return (
     <div className='container-game'>
 
+      <div className="container-game__board">
+
+      </div>
+
       <div className='list-tokens'>
+        <div className='list-tokens__group'>
         <Token left={5} right={4} key={1}/>
         <Token left={6} right={3} key={2}/>
         <Token left={3} right={5} key={3}/>
         <Token left={7} right={7} key={4}/>
         <Token left={1} right={0} key={5}/>
+        </div>
       </div>
       {/* <pre>
         {
