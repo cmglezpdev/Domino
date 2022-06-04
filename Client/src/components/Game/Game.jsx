e97949ef056cf80ef06a5352063aa9cc1b5c6b0b
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-06-03 23:33:26 -0400

    feat/client: Add styles of board

--- Client/src/components/Game/Game.jsx
+++ Client/src/components/Game/Game.jsx
@@ -9,16 +9,17 @@ export const Game = ({ settings }) => {
     <div className='container-game'>
 
       <div className="container-game__board">
-
-      </div>
+          <Token left={6} right={3} key={2} id={2} isBorad={true}/>
+          {/* <Token left={3} right={5} key={3} id={3} isBorad={true}/> */}
+      </div>      
 
       <div className='list-tokens'>
         <div className='list-tokens__group'>
-        <Token left={5} right={4} key={1}/>
-        <Token left={6} right={3} key={2}/>
-        <Token left={3} right={5} key={3}/>
-        <Token left={7} right={7} key={4}/>
-        <Token left={1} right={0} key={5}/>
+        <Token left={5} right={4} key={1} id={1} />
+        <Token left={6} right={3} key={2} id={2} />
+        <Token left={3} right={5} key={3} id={3} />
+        <Token left={7} right={7} key={4} id={4} />
+        <Token left={1} right={0} key={5} id={5} />
         </div>
       </div>
       {/* <pre>
--- Client/src/components/Game/Token.jsx
+++ Client/src/components/Game/Token.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import './token.scss';
 
 
-export const Token = ({ left, right }) => {
+export const Token = ({ left, right, id, isBorad }) => {
   return (
-    <div className='token'>
-        <div className='token--left vertex'><span>{left}</span></div>
-        <div className='token--right vertex'><span>{right}</span></div>
+    <div className={`token--${ isBorad ? 'board' : 'hand' } token-${id}`}>
+        <div className='left vertex'><span>{left}</span></div>
+        <div className='right vertex'><span>{right}</span></div>
     </div>
   )
 }
