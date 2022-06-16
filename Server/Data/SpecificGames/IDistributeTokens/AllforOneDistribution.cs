a6307bfaa81e3b1d2ca30a2e61542778a96ff7a3
Jorge Aspiolea <aspiolea28@gmail.com>
2022-06-16 15:33:16 -0400

    Change AllforOneDistribution

--- Server/Data/SpecificGames/IDistributeTokens/AllforOneDistribution.cs
+++ Server/Data/SpecificGames/IDistributeTokens/AllforOneDistribution.cs
@@ -6,33 +6,41 @@ public class AllforOneDistribution : IDistributeTokens {
         Random r = new Random();
         players = players.OrderBy(x => r.Next()).ToArray();
         
-        int count = 0;
         bool[] mask = new bool[10];
-    
+
+        int IntMax = 0;
+        int mark = tokens[0].right.Item1;
+        for(int i = 1; i < tokens.Count; i++){
+            if(tokens[i].right.Item1 == mark || tokens[i].left.Item1 == mark){
+                IntMax++;
+            }
+        }
+        List<List<Token>> hands = new List<List<Token>>();
+        
         for(int i = 0; i < players.Length; i++){
             List<Token> aux = new List<Token>();
-            int auxI = r.Next(0,9);
+            int auxI = r.Next(0,IntMax);
             while(mask[auxI]){
-                auxI = r.Next(0,9);
+                auxI = r.Next(0,IntMax);
             }
             mask[auxI] = true;
             for(int j = 0; j < tokens.Count; j++){
                 if(tokens[j].right.Item1 == auxI || tokens[j].left.Item1 == auxI){
                     aux.Add(tokens[j]);
                     tokens.RemoveAt(j);
-                    count++;
                     j--;
                 }
             }
-            while(count < 10) {
-                int aux2 = r.Next(0, tokens.Count);
-                aux.Add(tokens[aux2]);
-                tokens.RemoveAt(aux2);
-                count++;    
-                }
-            count = 0;
-            players[i].MakeTokens(aux);
+            hands.Add(aux);    
         }
+        for(int i = 0; i < hands.Count; i++){
+            while(hands[i].Count < 10) {
+                int aux2 = r.Next(0, tokens.Count);
+                hands[i].Add(tokens[aux2]);
+                tokens.RemoveAt(aux2);   
+                }
+            players[i].MakeTokens(hands[i]);
+        } 
         return players;
     }
 }
