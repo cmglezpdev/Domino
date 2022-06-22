85880081c977b593015342755ac64e827c23d2e0
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-06-22 09:59:29 -0400

    feat/add: styles to player data

---
+++ Client/src/helpers/dataPlayers.js
@@ -0,0 +1,6 @@
+
+import player_1 from '../images/hipster.png';
+
+export const iconsPlayers = [player_1, player_1, player_1, player_1, player_1, player_1, player_1, player_1, ];
+export const namesPlayers = ["Roberto", "Juan", "Carlos", "Daniel", "Jose", "Abel", "Mario", "Omar", "Felipe", "Agustin", "Fernando"];
+
