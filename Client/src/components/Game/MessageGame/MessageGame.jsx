151beb51283d28c00c7440abf4bc6c2d9de14b9d
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-06-24 13:26:38 -0400

    feat/add: change style of passed player

--- Client/src/components/Game/MessageGame/MessageGame.jsx
+++ Client/src/components/Game/MessageGame/MessageGame.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+
+import { namesPlayers } from '../../../helpers/dataPlayers';
 import './messageGame.scss'
 
 export const MessageGame = ({ currentPlay }) => {
   return (
     <>
-        {
+        {/* {
           currentPlay?.finishGame &&  (<div className='alert-player-game finished'>
                 <span className='alert'>El juego ha terminado...</span>
                 <span className='alert'>Lista de Ganadores:</span>
@@ -19,7 +21,14 @@ export const MessageGame = ({ currentPlay }) => {
             currentPlay?.passed && (<div className='alert-player-game passed'>
             <span>El jugador # { currentPlay?.currentPlayer } se paso!</span>
             </div>)
-        }
+        } */}
+
+
+<div className='alert-player-game passed'>
+            <span>{ namesPlayers[currentPlay?.currentPlayer || 0] } se ha pasado!</span>
+            </div>
+
+
     </>
     );
 }
