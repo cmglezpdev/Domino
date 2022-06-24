66e3d592fd5a31ecd113de8df349c9f4c9b9ae43
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-06-24 13:34:10 -0400

    feat/add styles of the list of player that won

--- Client/src/components/Game/MessageGame/MessageGame.jsx
+++ Client/src/components/Game/MessageGame/MessageGame.jsx
@@ -6,7 +6,7 @@ import './messageGame.scss'
 export const MessageGame = ({ currentPlay }) => {
   return (
     <>
-        {/* {
+        {
           currentPlay?.finishGame &&  (<div className='alert-player-game finished'>
                 <span className='alert'>El juego ha terminado...</span>
                 <span className='alert'>Lista de Ganadores:</span>
@@ -19,16 +19,9 @@ export const MessageGame = ({ currentPlay }) => {
         }
         {
             currentPlay?.passed && (<div className='alert-player-game passed'>
-            <span>El jugador # { currentPlay?.currentPlayer } se paso!</span>
+            <span>{  namesPlayers[ currentPlay?.currentPlayer ] } se ha pasado!</span>
             </div>)
-        } */}
-
-
-<div className='alert-player-game passed'>
-            <span>{ namesPlayers[currentPlay?.currentPlayer || 0] } se ha pasado!</span>
-            </div>
-
-
+        }
     </>
-    );
+  );
 }
