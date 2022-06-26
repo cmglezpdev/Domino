e42ed2634d76bf4a364318c28ff4a5272d87a6e5
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-06-26 01:10:06 -0400

    feat/add: styles to the boad

--- Client/src/components/Game/Board/Board.jsx
+++ Client/src/components/Game/Board/Board.jsx
@@ -6,19 +6,73 @@ import './board.scss';
 export const Board = ({ currentPlay }) => {
 
   const [gridSettings, setGridSettings] = useState({});
+  const [board, setBoard] = useState([]);
 
   useEffect(() => {
     if( currentPlay?.tokensInBoard ) {
       const tokens = currentPlay?.tokensInBoard;
-      console.log(tokens);
+      
       // count of rows and columns
       const height = tokens.length;
       const width = (height > 0) ? tokens[0].length : 0;
       console.log(height, width);
+      
+      const standardHeight = 10;
+      let auxBoard = new Array( Math.max(standardHeight, height) );
+      
+      let h = 0;
+      if( height < standardHeight ) {
+        let dif = (standardHeight - height) / 2;
+        
+        for(h = 0; h < dif; h ++) {
+          auxBoard[h] = new Array(width);
+          for(let j = 0; j < width; j ++) {
+            auxBoard[h][j] = <div>Vacio</div>;
+          }
+        }
+      }
 
+
+      for(let i = 0; i < height; i ++, h ++) {
+        auxBoard[h] = new Array(width);
+      
+        for(let j = 0; j < width; j ++) {  
+          if( tokens[i][j] !== null ) {
+
+              let direction = "";
+              if( tokens[i][j - 1] !== null || tokens[i][j + 1] !== null ) {
+                if( tokens[i][j].left === tokens[i][j].right ) direction = "vertical";
+                else direction = "horizontal";
+              } else {
+                direction = "horizontal";
+              }
+
+            auxBoard[h][j] = <Token 
+                left={tokens[i][j].left} 
+                right={tokens[i][j].right} 
+                key={ generateId() } 
+                id={ generateId() } 
+                direction={ direction }
+                visible="true"
+              />
+          } else {
+            auxBoard[h][j] = <div>Vacio</div>
+          }
+        }
+      }
+      
+      for( ; h < Math.max(standardHeight, height); h ++) {
+        auxBoard[h] = new Array(width);
+        for(let j = 0; j < width; j ++) {
+          auxBoard[h][j] = <div>Vacio</div>;
+        }
+      }
+
+      setBoard(auxBoard);
+      
       setGridSettings({
-          gridTemplateRows: `repeat(${height}, 100px)`,
-          gridTemplateColumns: `repeat(${width}, 100px)`
+          gridTemplateRows: `repeat(${Math.max(height, standardHeight)}, 100px)`,
+          gridTemplateColumns: `repeat(${width}, 100px)`,
       });
 
     }
@@ -29,21 +83,8 @@ export const Board = ({ currentPlay }) => {
     <div className="container-game__board" style={gridSettings}>
  
     {            
-      currentPlay?.tokensInBoard.map((level) => {
-        const row = level.map((token, index) => {
-          return (
-            <Token 
-              left={token.left} 
-              right={token.right} 
-              key={ generateId() } 
-              id={index} 
-              direction={( token.left !== token.right ) ? "horizontal" : "vertical"}
-              visible="true"
-            />)
-          }) 
-          return row;
-      })
-     }
+      board  
+    }
    </div> 
 
 
