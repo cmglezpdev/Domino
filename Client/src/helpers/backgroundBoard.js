2eb9f9efcbb7714bfe0e267623176429249e2cb5
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-06-30 01:36:41 -0400

    fix visual errores

--- Client/src/helpers/backgroundBoard.js
+++ Client/src/helpers/backgroundBoard.js
@@ -4,7 +4,7 @@ import background_2 from '../images/casino_bg_2.jpg'
 import background_3 from '../images/casino_bg_3.jpg'
 import background_4 from '../images/casino_bg_4.jpg'
 
-const TotalBackgrounds = 9;
+const TotalBackgrounds = 5;
 let background = -1;
 const bgCasino = [background_0, background_1, background_2, background_3, background_4];
 
--- Client/src/helpers/dataPlayers.js
+++ Client/src/helpers/dataPlayers.js
@@ -1,6 +1,10 @@
 
-import player_1 from '../images/hipster.png';
+import player_1 from '../images/player_1.png';
+import player_2 from '../images/player_2.png';
+import player_3 from '../images/player_3.png';
+import player_4 from '../images/player_4.png';
+import player_5 from '../images/player_5.png';
 
-export const iconsPlayers = [player_1, player_1, player_1, player_1, player_1, player_1, player_1, player_1, ];
+export const iconsPlayers = [player_1, player_2, player_3, player_4, player_5, player_1, player_2, player_3, ];
 export const namesPlayers = ["Roberto", "Juan", "Carlos", "Daniel", "Jose", "Abel", "Mario", "Omar", "Felipe", "Agustin", "Fernando"];
 
