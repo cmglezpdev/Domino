6ec9567b9314919d29b50a2e971f72a94367d6a0
Jorge Aspiolea <aspiolea28@gmail.com>
2022-07-01 00:31:52 -0400

    Add Rareproperties

---
+++ Server/Data/SpecificGames/TokenValue/RareProperties.cs
@@ -0,0 +1,35 @@
+namespace Server.Data.Classes;
+
+public class RareProperties : TokenValue {
+    public override int Value(Token token)
+    {
+        Func<int,int,int>[] calculator = {a,b,c,d,e,f,g};
+        Random r = new Random();
+        int left = token.Left.Value;
+        int right = token.Right.Value;
+        int ramdom = r.Next(0,calculator.Length - 1);
+        return calculator[ramdom](right,left);
+
+    }
+    private int a(int right, int left){
+        return (int)Math.Pow(right,left);
+    }
+    private int b(int right, int left){
+        return (right + left)/2;
+    }
+    private int c(int right, int left){
+        return (int)Math.Abs((Math.Cos(left)-Math.Sin(right))*10);
+    }
+    private int d(int right, int left){
+        return Math.Abs((right + left)*(right-left));
+    }
+    private int e(int right, int left){
+        return (int)((Math.Sqrt(right) + Math.Log2(left))*5);
+    }
+    private int f(int right, int left){
+        return 0;
+    }
+    private int g(int right, int left){
+        return 1000000;
+    }
+}
