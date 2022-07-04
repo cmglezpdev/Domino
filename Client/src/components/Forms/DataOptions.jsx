e14e3a070b643a0145565c1506f1c8cbad2d827d
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-07-04 19:25:20 -0400

    feat/complete select type of players

--- Client/src/components/Forms/DataOptions.jsx
+++ Client/src/components/Forms/DataOptions.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Dropdown, Input } from 'semantic-ui-react';
 import { SettingsContext } from '../../helpers/SettingsContext';
 import generateId from '../../helpers/generateIds';
+import { namesPlayers } from '../../helpers/dataPlayers';
 
 export const DataOptions = ({ titleOption, nameOptions, id, value, GetOptions }) => {
 
@@ -10,10 +11,13 @@ export const DataOptions = ({ titleOption, nameOptions, id, value, GetOptions })
     const TypeOptions = GetTypeOptions(nameOptions);
     
     const handleChange = (e, result) => {
+        let aux = settings.player;
+        if( id == "countPlayers" ) aux = new Array(parseInt(GetOptions("countPlayers")[result.value])).fill(0);
 
         setSettings({
             ...settings,
             [id]: result.value,
+            player: aux
         });
     }
 
@@ -32,7 +36,7 @@ export const DataOptions = ({ titleOption, nameOptions, id, value, GetOptions })
                 getArrayPlayers( parseInt(GetOptions("countPlayers")[settings.countPlayers]) ).map(player => {
                     return (
                         <>
-                            <h4>Jugador {player}</h4>
+                            <h4>Jugador: { namesPlayers[player] }</h4>
                             <Dropdown
                                 placeholder='Seleccionar'
                                 fluid
@@ -40,12 +44,14 @@ export const DataOptions = ({ titleOption, nameOptions, id, value, GetOptions })
                                 selection
                                 options={TypeOptions}
                                 onChange={(e, result) => {
+                                    let aux = settings.player;
+                                    aux[player] = result.value;
                                     setSettings({
                                         ...settings,
-                                        [`player_${player}`]: result.value,
+                                        player: aux,
                                     });
                                 }}
-                                value={ settings[`player_${player}`] }
+                                value={ settings[id][player] }
                             />
                         </>
                     )
