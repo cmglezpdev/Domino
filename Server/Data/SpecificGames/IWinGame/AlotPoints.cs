317121447d21e55fe10fd29e9ac52ace058e9d1d
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-07-09 00:42:54 -0400

    commit with description of AlotPlayer.cs

--- Server/Data/SpecificGames/IWinGame/AlotPoints.cs
+++ Server/Data/SpecificGames/IWinGame/AlotPoints.cs
@@ -3,19 +3,15 @@ using Server.Data.Interfaces;
 public class ALotPoints : IWinGame {
     public IWinGame Clone() => new ALotPoints();
 
-    // TODO: Si alguien se pega entonces este va a ser el primero siempre.
+    // El que mas punto tenga gana, pero si alguien se pega gana entoces el
     public IEnumerable<PlayerInfo> GetWinnersGame( IBoard board, IEnumerable<PlayerInfo> players ) {
         List< PlayerInfo > winners = new List<PlayerInfo>();
         foreach( var ply in players ) winners.Add(ply);
 
         winners.Sort((a, b) => {
-            int Ida = a.IDPlayer.Item1;
-            int Idb = b.IDPlayer.Item1;
-
-            if( Manager.CountTokenByPlayers[ Manager.SearchPlayerIndex(Ida) ] == 0 ) return -1;
-            if( Manager.CountTokenByPlayers[ Manager.SearchPlayerIndex(Idb) ] == 0 ) return 1;
-            
-            return ( a.Points > b.Points ) ? -1 : (a.Points > b.Points) ? 1 : 0;
+            if( a.Points == 0 ) return -1;
+            if( b.Points == 0 ) return 1;
+            return ( a.Points > b.Points ) ? -1 : (a.Points < b.Points) ? 1 : 0;
         });
 
 
